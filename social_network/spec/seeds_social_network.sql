89ed4722ed4cb54baf9c48fa12f9e2698c118afb
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-23 12:16:18 +0000

    created test database seeds

---
+++ social_network/spec/seeds_social_network.sql
@@ -0,0 +1,22 @@
+TRUNCATE TABLE users RESTART IDENTITY CASCADE; 
+-- TRUNCATE TABLE posts RESTART IDENTITY; 
+
+INSERT INTO 
+  users (username, email_address) 
+  VALUES ('Jo1234', 'jo1234@fakeemail.co.uk');
+INSERT INTO 
+  users (username, email_address) 
+  VALUES ('Bloggs1234', 'bloggs4567@fakeemail.co.uk');
+
+INSERT INTO 
+  posts (title, content, number_of_views, user_id) 
+  VALUES ('post title 1', 'post content 1', 1, 1);
+INSERT INTO 
+  posts (title, content, number_of_views, user_id) 
+  VALUES ('post title 2', 'post content 2', 2, 1);
+INSERT INTO 
+  posts (title, content, number_of_views, user_id) 
+  VALUES ('post title 3', 'post content 3', 3, 2);
+INSERT INTO 
+  posts (title, content, number_of_views, user_id) 
+  VALUES ('post title 4', 'post content 4', 4, 2);
